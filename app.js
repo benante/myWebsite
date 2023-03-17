d1d94d62fd052ce3abbb8a83cc1572f91620a96d
benante <tommaso.orlandi3@gmail.com>
2023-03-17 22:31:38 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector("header");
+  const intro = document.querySelector(".intro");
+  const headerHeight = header.offsetHeight;
+  console.log(header);
+  console.log(intro);
+  intro.style.height = "calc(100vh - " + headerHeight + "px)";
+});
