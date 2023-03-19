1dd9a7e25c11c92815e805129d587011f2ee4c61
benante <tommaso.orlandi3@gmail.com>
2023-03-19 22:58:21 +0000

    Third commit, added some style and new elements: about section, cards, contact and navbar on the side

--- app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const header = document.querySelector("header");
-  const intro = document.querySelector(".intro");
-  const headerHeight = header.offsetHeight;
-  console.log(header);
-  console.log(intro);
-  intro.style.height = "calc(100vh - " + headerHeight + "px)";
-});
-const sections = document.querySelectorAll("section");
